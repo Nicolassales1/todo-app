4477a4f93476362f9ee302e6a267fd3e7617c551
Nikaosales <nicolassalesdefaria2000@gmail.com>
2023-11-30 15:03:23 -0300

    primeira execução do servidor

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const express = require("express")
+const express = require("express");
+const { request } = require("http");
 
 const app = express()
 
+app.get('/', (req, res) =>{
+    res.send("Ola mundo")
+})
+
+app.listen(3000, ()=>{
+    console.log("servidor rodando na porta 3000!")
+})
