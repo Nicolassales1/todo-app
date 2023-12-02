10a02fad5aa60da33fe1de17100575477ae7c960
Nikaosales <nicolassalesdefaria2000@gmail.com>
2023-12-02 18:25:20 -0300

    exeibição das tarefas na pagina home concluida

--- index.js
+++ index.js
@@ -56,9 +56,8 @@ app.get('/', (requisicao, resposta) => {
             };
         });
 
-        
+        resposta.render('home', { tarefas });
     });
-    resposta.render('home');
 });
 
 const conexao = mysql.createConnection({
